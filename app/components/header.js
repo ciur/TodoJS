367a81651b1049a3ed290cfb6cea9e514dc8fa63
Eugen Ciur <eugen@papermerge.com>
2023-02-03 08:53:12 +0100

    add class based component/remove GH Ci related files

---
+++ app/components/header.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class HeaderComponent extends Component {
+
+    @action
+    onKeyDown({ target, key }) {
+      let text = target.value.trim();
+      let hasValue = Boolean(text);
+  
+      if (key === 'Enter' && hasValue) {
+        alert(text);
+  
+        target.value = ''
+      }
+    }
+}
